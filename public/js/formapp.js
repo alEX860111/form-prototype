6deab126f6b35aaa472aab1d1c43ffff88ededc1
Alexander Buhr <alexander.buhr@hypoport.de>
2014-07-24 10:08:56 +0200

    formated code

--- public/js/formapp.js
+++ public/js/formapp.js
@@ -1,30 +1,30 @@
 var app = angular.module('formApp', ['controllers', 'ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider
-        .state('form', {
-            url: '/form',
-            templateUrl: 'form.html',
-            controller: 'formController'
-        })
-        
-        .state('form.pdata', {
-            url: '/pdata',
-            templateUrl: 'form-pdata.html',
-            step: 1
-        })
-        
-        .state('form.advanced', {
-            url: '/advanced',
-            templateUrl: 'form-advanced.html',
-            step: 2
-        })
-        
-        .state('form.confirm', {
-            url: '/confirm',
-            templateUrl: 'form-confirm.html',
-            step: 3
-        });
+  $stateProvider
+    .state('form', {
+      url: '/form',
+      templateUrl: 'form.html',
+      controller: 'formController'
+    })
+
+    .state('form.pdata', {
+      url: '/pdata',
+      templateUrl: 'form-pdata.html',
+      step: 1
+    })
+
+    .state('form.advanced', {
+      url: '/advanced',
+      templateUrl: 'form-advanced.html',
+      step: 2
+    })
+
+    .state('form.confirm', {
+      url: '/confirm',
+      templateUrl: 'form-confirm.html',
+      step: 3
+    });
 
     $urlRouterProvider.otherwise('/form/pdata');
 });
