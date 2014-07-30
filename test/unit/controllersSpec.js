bc04f694ccf401b3212cea6d31762ac19860c37f
Alexander Buhr <alexander.buhr@hypoport.de>
2014-07-30 18:20:02 +0200

    adjusted test

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,36 +1,32 @@
 describe('formController', function() {
 
+  var scope;
+
   beforeEach(function() {
     module('controllers');
   });
 
-  it('should provide an empty formData object', inject(function($rootScope, $controller) {
-    var scope = $rootScope.$new();
-    var ctrl = $controller('formController', {
+  beforeEach(inject(function($rootScope, $controller) {
+    scope = $rootScope.$new();
+    $controller('formController', {
       $scope: scope
     });
+  }));
+
+  it('should provide an empty formData object', function() {
     expect(scope.formData).toEqual({});
-  }));
+  });
 
-  it('should provide a function to process the submitted data', inject(function($rootScope, $controller) {
-    var scope = $rootScope.$new();
-    var ctrl = $controller('formController', {
-      $scope: scope
-    });
+  it('should provide a function to process the submitted data', function() {
     expect(scope.processForm).toBeDefined();
-  }));
+  });
 
-  it('processForm should clear the formData', inject(function($rootScope, $controller) {
-    var scope = $rootScope.$new();
-    var ctrl = $controller('formController', {
-      $scope: scope
-    });
-
+  it('processForm should clear the formData', function() {
     scope.formData = {
       name: 'test'
     };
     scope.processForm();
     expect(scope.formData).toEqual({});
-  }));
+  });
 
 });
