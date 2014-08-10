ed3a307567393e317ede8971f428b7815c142e2e
alEX860111 <alex86011119@gmail.com>
2014-08-10 14:23:45 +0200

    modularizing the form (WIP)

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -10,16 +10,17 @@ app.controller('formController', function($scope) {
 
   $scope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams) {
     console.dir($scope);
-    if (toState.step > fromState.step && $scope.form.$invalid) {
-      for (input in $scope.form) {
-        if (input.charAt(0) != '$' && $scope.form[input].$pristine) {
-          $scope.form[input].$setTouched();
-          $scope.form[input].$dirty = true;
-          $scope.form[input].$pristine = false;
-        }
-      }
-      event.preventDefault();
-    }
+    event.preventDefault();
+    // if (toState.step > fromState.step && $scope.form.$invalid) {
+    //   for (input in $scope.form) {
+    //     if (input.charAt(0) != '$' && $scope.form[input].$pristine) {
+    //       $scope.form[input].$setTouched();
+    //       $scope.form[input].$dirty = true;
+    //       $scope.form[input].$pristine = false;
+    //     }
+    //   }
+    //   event.preventDefault();
+    // }
   });
 
 });
--- public/js/directives.js
+++ public/js/directives.js
@@ -1,13 +1,37 @@
 var app = angular.module('directives', []);
 
-app.directive('textfield', function() {
+app.directive('interpolForm', function($interpolate) {
+  return {
+    restrict: 'AC',
+    priority: 10000,
+    controller: function($scope, $element, $attrs) {
+      $attrs.$set('name', $interpolate($attrs.interpolForm || $attrs.name)($scope));
+      $element.data('$interpolFormController', null);
+    }
+  };
+});
+
+app.directive('interpolModel', function($interpolate) {
+  return {
+    priority: 10000,
+    restrict: 'AC',
+    controller: function($scope, $element, $attrs) {
+      $attrs.$set('name', $interpolate($attrs.interpolModel || $attrs.name)($scope));
+      $element.data('$interpolModelController', null);
+    }
+  };
+});
+
+app.directive('customTextfield', function() {
   return {
     restrict: 'A',
+    templateUrl: 'form-textfield.html',
     scope: {
-      label: '@',
-      datafield: '@',
-      ngModel: '='
-    },
-    templateUrl: 'form-textfield.html'
+      form: '=form',
+      data: '=data',
+      type: '@type',
+      name: '@name',
+      label: '@label'
+    }
   };
 });
